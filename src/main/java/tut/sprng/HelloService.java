605f9f908daf174114bfd5dedaf1611dfeb6d493
Simion Ilie <simion.iliecostin@gmail.com>
2015-07-29 21:14:46 +0300

    first commit

---
+++ src/main/java/tut/sprng/HelloService.java
@@ -0,0 +1,10 @@
+package tut.sprng;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloService {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
