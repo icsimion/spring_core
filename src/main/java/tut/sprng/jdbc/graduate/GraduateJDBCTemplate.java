f940bc46ad4f6a01bf648c5bd7888e4f4846ed50
Simion Ilie <simion.iliecostin@gmail.com>
2015-08-01 20:18:45 +0300

    updated GraduateJDBCTemplate.java

--- src/main/java/tut/sprng/jdbc/graduate/GraduateJDBCTemplate.java
+++ src/main/java/tut/sprng/jdbc/graduate/GraduateJDBCTemplate.java
@@ -8,46 +8,45 @@ import tut.sprng.jdbc.specialization.Specialization;
 
 public class GraduateJDBCTemplate implements GraduateDAO {
 	private DataSource dataSource;
-	// standard JdbcTemplate
-	private JdbcTemplate jdbcTemplateObject;
+	private JdbcTemplate jdbcTemplate;
 
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
-		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
 	public void create(int id, String name, int age, int score, Specialization specialization) {
 		String SQL = "insert into Graduate (id, name, age, score, specId) values (?, ?, ?, ?, ?)";
 
 		Integer specId = specialization == null ? null : specialization.getId();
-		jdbcTemplateObject.update(SQL, id, name, age, score, specId);
+		jdbcTemplate.update(SQL, id, name, age, score, specId);
 		System.out.println("Created Record Name = " + name + " Age = " + age);
 		return;
 	}
 
 	public Graduate getGraduate(int id) {
 		String SQL = "select * from graduate where id = ?";
-		Graduate graduate = jdbcTemplateObject.queryForObject(SQL,
+		Graduate graduate = jdbcTemplate.queryForObject(SQL,
 				new Object[]{id}, new GraduateMapper());
 		return graduate;
 	}
 
 	public List<Graduate> listGraduates() {
 		String SQL = "select * from graduate";
-		List<Graduate> graduates = jdbcTemplateObject.query(SQL,
+		List<Graduate> graduates = jdbcTemplate.query(SQL,
 				new GraduateMapper());
 		return graduates;
 	}
 
 	public void delete(Integer id) {
 		String SQL = "delete from Graduate where id = ?";
-		jdbcTemplateObject.update(SQL, id);
+		jdbcTemplate.update(SQL, id);
 		System.out.println("Deleted Record with ID = " + id);
 	}
 
 	public void update(Integer id, Integer age) {
 		String SQL = "update Graduate set age = ? where id = ?";
-		jdbcTemplateObject.update(SQL, age, id);
+		jdbcTemplate.update(SQL, age, id);
 		System.out.println("Updated Record with ID = " + id);
 	}
 
